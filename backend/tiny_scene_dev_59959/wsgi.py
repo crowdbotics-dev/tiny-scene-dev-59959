a12e3ad56b8cdef2555c257d5a7ecd959dedfa78
Crowdbotics <engineering@crowdbotics.com>
2023-01-30 08:25:40 +0000

    Initial commit.

---
+++ backend/tiny_scene_dev_59959/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tiny_scene_dev_59959 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tiny_scene_dev_59959.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-scene-dev-59959",
+  url: "https://tiny-scene-dev-59959.botics.co",
+  api: "https://tiny-scene-dev-59959.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
